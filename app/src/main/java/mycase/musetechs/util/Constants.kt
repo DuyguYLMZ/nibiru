53837656fc3c6e915d4a82e209258ee08f110ad0
duyguyilmaz <duyguyilmazd@gmail.com>
2022-06-12 14:52:12 +0300

    Revert "first commit"
    This reverts commit 97a2e521

---
+++ app/src/main/java/mycase/musetechs/util/Constants.kt
@@ -0,0 +1,16 @@
+package atlasyazilim.nto.biletplaza.util
+
+object Constants {
+    const val BASE_URL ="https://api.themoviedb.org/4/"
+
+    const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/"
+    const val IMAGE_W92 = "w92"
+    const val IMAGE_W154 = "w154"
+    const val IMAGE_W185 = "w185"
+    const val IMAGE_W342 = "w342"
+    const val IMAGE_W500 = "w500"
+    const val IMAGE_W780 = "w780"
+
+    const val API_KEY = "6c1d9d4879c6a82d5f72e0cddc952722"
+    const val CONNECT_ERROR = "Bağlantıda problem oldu. Lütfen daha sonra tekrar deneyiniz."
+}
